723334fbb3cf7d5b107c90c562b0c9f38dfcffb3
Joe Grace <joeggrace@gmail.com>
2020-05-13 22:28:55 -0400

    All iterations complete

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -1,38 +1,64 @@
+import javax.sound.midi.Soundbank;
+import java.util.Random;
 import java.util.Scanner;
 
 public class GuessingGameApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(11);
+
         System.out.println("Welcome to the Guessing Game!");
-        System.out.println("Please enter a number between 1-10:");
+        System.out.println("You have 3 Guesses");
+        System.out.println("Please pick a number between 1-10:");
         Scanner input = new Scanner(System.in);
-        int guess = input.nextInt();
-        if(guess == 7) {
-            System.out.println("Congratulations you have won!");
-        }
-        else if(guess == 0){
-            System.out.println("You have entered 0. Please enter a number between 1-10:");
-            guess = input.nextInt();
-            if(guess == 7){
-                System.out.println("Congratulations you have won!");
-                            }
-            else{
-                System.out.println("You Have Guessed Incorrectly");
+        int numberGuess = input.nextInt();
+        int numberOfGuesses = 1;
+        for ( ; numberOfGuesses < 3 ; numberOfGuesses++){
+            if (numberGuess == randomNumber) {
+                System.out.println("Congratulations, You have guessed correctly.");
+                System.out.println("You have won!");
+                break;
             }
-                    }
-        else {
-            System.out.println("You Have Guessed Incorrectly");
-        }
-        if (guess != 7){
-            System.out.println("Please try another number:");
-            guess = input.nextInt();
-            if (guess == 7){
-                System.out.println("You have Won!");
+            else if(numberGuess == -1){
+                System.out.println("You have entered -1");
+                System.out.println("You Lose");
+                break;
+            }
+            else if (numberGuess == 0) {
+                System.out.println("You have entered 0. Please enter a number between 1-10:");
+                numberGuess = input.nextInt();
             }
             else{
-                System.out.println("You have Lost.");
+                if(numberGuess < randomNumber){
+                    System.out.println("You guessed " + numberGuess + ":");
+                    System.out.println("The correct number is more than " + numberGuess + ":");
+                    System.out.println("Guess again. You have " + (3 - numberOfGuesses) + " left:");
+                    numberGuess = input.nextInt();
+                }
+                else{
+                    System.out.println("You guessed " + numberGuess + ":");
+                    System.out.println("The correct number is less than " + numberGuess + ":");
+                    System.out.println("Guess again. You have " + (3 - numberOfGuesses) + " left:");
+                    numberGuess = input.nextInt();
+                }
+
+
+                }
             }
+        if (numberGuess != randomNumber && numberGuess != -1){
+            System.out.println("You have used all your guesses.");
+            System.out.println("You did not guess correctly.");
+            System.out.println("You lose");
         }
-    }
+        else if(numberGuess == randomNumber && numberOfGuesses == 3){
+            System.out.println("Congratulations, you have guessed correctly.");
+            System.out.println("You have won!");
+        }
+
+        }
+
+
+
 
 }
